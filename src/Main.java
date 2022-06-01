bcd306f991d20db26017ab968cb18934c058fa32
Olga Kapustina <hellog@rambler.ru>
2022-06-01 10:46:23 +0300

    add BonusMilesService file

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int balance = 150;
-        int cash1 = 100;
-        int bonus = cash1 / 100;
-        if (cash1 >= 1000) {
-            bonus = cash1 / 100;
-        } else {
-            bonus = 0;
-        }
-        System.out.println(bonus);
-        System.out.println(balance + cash1 + bonus);
-
-        int cash2 = 1475;
-        int bonus2 = cash2 / 100;
-        if (cash2 >= 1000) {
-            bonus2 = cash2 / 100;
-        } else {
-            bonus = 0;
-        }
-        System.out.println(bonus2);
-        System.out.println(balance + cash2 + bonus2);
+        BonusMilesService service = new BonusMilesService();
+        int price = 17_236;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
-
